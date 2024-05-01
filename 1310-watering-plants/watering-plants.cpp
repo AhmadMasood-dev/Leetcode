ac2e7dd9d02052eca8e13d71a38bd2cf21d85806
AhmadMasood-dev <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-05-01 21:53:56 +0500

    Time: 3 ms (67.78%) | Memory: 10.8 MB (44.17%) - LeetSync

---
+++ 1310-watering-plants/watering-plants.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int wateringPlants(vector<int>& plants, int capacity) {
+       int result = size(plants), can = capacity;
+        for (int i = 0; i < size(plants); ++i) {
+            if (can < plants[i]) {
+                result += 2 * i;
+                can = capacity;
+            }
+            can -= plants[i];
+        }
+        return result;  
+    }
+};
