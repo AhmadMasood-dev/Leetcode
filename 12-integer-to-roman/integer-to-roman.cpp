18fdc2405babf923fa75828e89c5b71085971ba4
Muhammad Adnan Sultan <114609178+madnansultandotme@users.noreply.github.com>
2024-10-30 11:45:23 +0500

    Time: 6 ms (40.30%) | Memory: 13.7 MB (28.59%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> cs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        vector<int> vs = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        string ans;
-        for (int i = 0; i < cs.size(); ++i) {
-            while (num >= vs[i]) {
-                num -= vs[i];
-                ans += cs[i];
+        // Roman symbols and corresponding integer values in descending order
+        vector<pair<int, string>> romanSymbols = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+        
+        string result = "";
+        
+        // Iterate through each symbol-value pair
+        for (const auto& [value, symbol] : romanSymbols) {
+            // Append symbols while num is large enough for the current value
+            while (num >= value) {
+                result += symbol;
+                num -= value;
             }
         }
-        return ans;
+        
+        return result;
     }
 };
